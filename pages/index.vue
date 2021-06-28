ed674a8394d763cccd2aafc0f575042860099ff0
Sac Le <le.sinh@mail.i-on.net>
2021-06-28 20:44:45 +0700

    update code

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="test">
+    dgdfgdfgfdg
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style lang="sass">
+.test
+  color: red
+</style>

9bc3289993dc797bb6f777c8f2a85cec3a81e018
Sac Le <le.sinh@mail.i-on.net>
2021-06-30 11:42:18 +0700

    g dfgdfg df g df gfdgdf  gfhfghgfhh

--- pages/images/index.vue
+++ pages/images/index.vue
@@ -2,11 +2,12 @@
   <div class="container">
     <div class="row">
       <div class="col-12">
-        <div id="mygallery" >
+        <div id="mygallery" infinite-scroll-disabled="autoLoadDisabled" >
           <a v-for="image in images" :key="image.url" :href="image.url">
             <img :src="image.url"/>
           </a>
         </div>
+        <infinite-loading @infinite="infiniteHandler"></infinite-loading>
         <a class="btn btn-success" href="javascript:;" @click="nextPage">{{ loadMoreText }}</a>
       </div>
     </div>
@@ -15,23 +16,39 @@
 
 <script>
 import xdata from './data/images.json'
+import InfiniteLoading from 'vue-infinite-loading';
+
 
 export default {
+  components: {
+    InfiniteLoading
+  },
   data () {
     return {
       allimages: [],
       images: [],
       offset: 0,
       perPage: 25,
-      loadMoreText: 'Load more'
+      loadMoreText: 'Load more',
+      loading: false,
     }
   },
   created() {
     this.allimages = this.shuffle(xdata)
     this.nextPage()
   },
+  computed: {
+    autoLoadDisabled() {
+      return this.loading || this.allimages.length === 0;
+    },
+  },
   methods: {
-    nextPage () {
+    infiniteHandler ($state) {
+      this.nextPage(() => {
+        $state.loaded();
+      })
+    },
+    nextPage (callback = null) {
       let ids = new Set(this.images.map(d => d.url))
       this.allimages = this.allimages.filter(d => !ids.has(d.url))
       this.images = [...this.images, ...this.allimages.slice(0, 25)];
@@ -41,6 +58,17 @@ export default {
           rowHeight : 300,
           lastRow : 'nojustify',
           margins : 3
+        }).on('jg.complete', function () {
+          if (callback != null) {
+            callback()
+          }
+          $(this).find('a').colorbox({
+            maxWidth : '80%',
+            maxHeight : '80%',
+            opacity : 0.8,
+            transition : 'elastic',
+            current : ''
+          });
         });
       })
     },
